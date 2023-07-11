fdd893d241fcc180818ccd4f54b51cddbbc42b24
AltinDurakuu <altinduraku404@gmail.com>
2023-07-11 19:12:39 +0200

    Added discover section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroBanner from "../components/HeroBanner";
+import DiscoverSection from "../components/DiscoverSection";
 
 function Home(){
     return (
         <main className="full-block">
             <HeroBanner />
+            <DiscoverSection />
         </main>
     )
 }
