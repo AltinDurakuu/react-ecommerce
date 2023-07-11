f586cfc19554171d3c15999d8015f400d2fc8b71
AltinDurakuu <altinduraku404@gmail.com>
2023-07-11 16:15:28 +0200

    added some basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './styles/App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default Navbar;
