d6cc6cc1b4ddc77f598141eda36a0347044a7f3c
AltinDurakuu <altinduraku404@gmail.com>
2023-07-13 18:20:49 +0200

    some naming changes

--- src/components/DiscoverSection.jsx
+++ src/components/DiscoverSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/DiscoverSection.css"
-import DiscoverCategory1 from "./DiscoverCategory1";
+import DiscoverBanner from "./DiscoverCategoryBanner";
 
 function DiscoverSection(){
     return (
@@ -9,18 +9,18 @@ function DiscoverSection(){
                 <div className="category-content">
                     <div className="category-content-left-side">
                         <div className="category-content-left-side-fashion">
-                            <DiscoverCategory1 name="Clothes" link="/clothes"/>
+                            <DiscoverBanner name="Clothes" link="/clothes"/>
                         </div>
                         <div className="category-content-left-side-best-sale big-category">
-                            <DiscoverCategory1 name="Popular Items" link="#popular_items"/>
+                            <DiscoverBanner name="Popular Items" link="#popular_items"/>
                          </div>
                     </div>
                     <div className="category-content-right-side">
                         <div className="category-content-right-side-accessories big-category">
-                                <DiscoverCategory1 name="Recent releases" link="#recent_releases"/>
+                                <DiscoverBanner name="Recent releases" link="#recent_releases"/>
                         </div>
                         <div className="category-content-right-side-others">
-                                <DiscoverCategory1 name="Accessories" link="/accessories"/>
+                                <DiscoverBanner name="Accessories" link="/accessories"/>
                         </div>      
                     </div>
             </div>
